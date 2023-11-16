f32ec8ea7dde827f054a2c947e4819f82e994ad0
FridIsar <isarfridriksson@gmail.com>
2023-11-16 20:36:53 +0100

    changed names localhost

--- src/components/NavBar/LoginButton.jsx
+++ src/components/NavBar/LoginButton.jsx
@@ -21,7 +21,7 @@ const LoginButton = () => {
     handleButtonClicked(false);
     try {
       // Make a request to the login API
-      const response = await fetch("http://localhost:3150/login", {
+      const response = await fetch("https://newtest-smoky.vercel.app/ogin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
